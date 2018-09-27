2d5f2846073b55c9cab1e3c8190fabc67c01208d
dimitarbez <dimitarbez@gmail.com>
2018-09-27 21:25:38 +0200

    26

---
+++ Assets/AdventureGame.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour {
+
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    int[] oddNumbers = { 1, 3, 5, 7, 9 };
+
+    State state;
+
+	// Use this for initialization
+	void Start () {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+        Debug.Log(oddNumbers[3]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ManageState();
+	}
+
+    private void ManageState()
+    {
+        State[] nextStates = state.GetNextStates();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+        }
+        textComponent.text = state.GetStateStory();
+    }
+}
